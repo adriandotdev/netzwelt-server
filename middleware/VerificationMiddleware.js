44553a3d522dc259db468328b835969537b9ca91
Adrian Nads <marcelonads@gmail.com>
2023-08-25 05:32:06 +0800

    removed unused variables

--- middleware/VerificationMiddleware.js
+++ middleware/VerificationMiddleware.js
@@ -4,8 +4,6 @@ module.exports = function VerificationMiddleware(req, res, next) {
 
     const token = req.cookies['auth-token'];
 
-    if (token === '') next();
-
     try {
 
         const isValid = jwt.verify(token, process.env.SECRET_KEY);
@@ -16,7 +14,6 @@ module.exports = function VerificationMiddleware(req, res, next) {
     }
     catch (err) {
 
-        console.log(err);
         return res.status(401).json({ message: 'Unauthorized' })
     }
 }
